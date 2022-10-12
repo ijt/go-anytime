61fcfe3d5de530df0e816f39ef1dd4c5fbc26a9d
Issac Trotts <issac.trotts@protonmail.com>
2022-10-11 21:01:54 -0600

    remove some unused funcs

--- anytime.go
+++ anytime.go
@@ -26,16 +26,6 @@ func (r Range) String() string {
 	return fmt.Sprintf("{time: %v, duration: %v}", r.Time, r.Duration)
 }
 
-// Start is the start of the range.
-func (r Range) Start() time.Time {
-	return r.Time // not sure about the exact syntax here
-}
-
-// End is the end of the range.
-func (r Range) End() time.Time {
-	return r.Start().Add(r.Duration)
-}
-
 type direction int
 
 const (
