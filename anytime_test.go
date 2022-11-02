05105909442d481ea89e592726cffa64e7ef2696
Issac Trotts <issac.trotts@protonmail.com>
2022-11-02 13:37:35 -0600

    make error msg clearer in TestPartitionTimes()

--- anytime_test.go
+++ anytime_test.go
@@ -1239,7 +1239,7 @@ func TestPartitionTimes(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := PartitionTimes(tt.args.s, tt.args.ref, tt.args.options...); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("PartitionTimes() = %v, want %v", got, tt.want)
+				t.Errorf("PartitionTimes() = %#v, want %#v", got, tt.want)
 			}
 		})
 	}
