4ef849e86ca5451d403b99e3397b5764cb68afb7
Issac Trotts <issac.trotts@protonmail.com>
2023-01-02 11:33:56 -0700

    add comment to ReplaceAllRangesByFunc

--- v2/replace.go
+++ v2/replace.go
@@ -12,6 +12,18 @@ const (
 	Past
 )
 
+// ReplaceAllRangesByFunc replaces all dates and date ranges within the string
+// s by the result of calling f on the parsed date/range and the substring
+// src that gave rise to it.
+//
+// Ranges include things like "this year". That is the range from Jan 1 to Dec
+// 31 of the current year.
+//
+// Ranges also include things like "from last year to next year" or
+// "last year to next year".
+//
+// In ambiguous cases like "December" that could be in the past or the future,
+// the dir argument tells whether to choose the Past or Future instance of it.
 func ReplaceAllRangesByFunc(s string, now time.Time, dir Direction, f func(src string, r Range) string) (string, error) {
 	ls := strings.ToLower(s)
 	var parts []string
