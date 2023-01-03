1e110b9e3fbd28c8e4f1535eeb9b7065c06a9efe
Issac Trotts <issac.trotts@protonmail.com>
2023-01-02 21:02:11 -0700

    add implementation of fixUTF8()

--- v2/utf8.go
+++ v2/utf8.go
@@ -1,5 +1,22 @@
 package anytime
 
+import "unicode/utf8"
+
+// fixUTF8 salvages the valid UTF8 runes in the given string.
+// https://gist.github.com/scbizu/23a7d3f3436a35ab3b79c3cce91ca8fe
 func fixUTF8(s string) string {
-	return s
+	if utf8.Valid([]byte(s)) {
+		return s
+	}
+	rs := make([]rune, 0, len(s))
+	for i, r := range s {
+		if r == utf8.RuneError {
+			_, size := utf8.DecodeRuneInString(s[i:])
+			if size == 1 {
+				continue
+			}
+		}
+		rs = append(rs, r)
+	}
+	return string(rs)
 }
